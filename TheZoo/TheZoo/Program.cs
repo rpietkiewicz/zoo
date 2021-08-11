a74639391689aa38d1910a8c77430a6e5799b36e
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-11 21:45:44 +1000

    Add some messages to display on the console and commit some small config changes

--- TheZoo/TheZoo/Program.cs
+++ TheZoo/TheZoo/Program.cs
@@ -6,7 +6,21 @@ namespace TheZoo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("###################################################\n");
+            Console.WriteLine("----------You are now using Zoo--------------\n");
+            Console.WriteLine("###################################################\n");
+            Console.WriteLine("**********Press any key to continue****************\n");
+            Console.ReadLine();
+            Console.WriteLine("Here are the list of animals that are available to see today: \n");
+            Console.WriteLine("/////Animal/////         " + "/////Location/////         ");
+            Console.WriteLine("1. Scorpion              " + "Desert East Side           ");
+            Console.WriteLine("2. Lion                  " + "Plains West Side           ");
+            Console.WriteLine("3. Grizzly Bear          " + "Forest South Side          ");
+            Console.WriteLine("4. Cheetah               " + "Plains East Side           ");
+            //Console.WriteLine("Enter in a animal to see the time of that animals exhibition");
+            //Console.ReadLine();
+            //Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
