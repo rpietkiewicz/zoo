fa2f53a4ae6ca70c40e583fa3b81fc9a21bdd824
rpietkiewicz <35366815+rpietkiewicz@users.noreply.github.com>
2021-08-12 23:19:39 +1000

    Add some console messages and add a new variable

--- TheZoo/TheZoo/Program.cs
+++ TheZoo/TheZoo/Program.cs
@@ -17,8 +17,15 @@ namespace TheZoo
             Console.WriteLine("2. Lion                  " + "Plains West Side           ");
             Console.WriteLine("3. Grizzly Bear          " + "Forest South Side          ");
             Console.WriteLine("4. Cheetah               " + "Plains East Side           ");
-            //Console.WriteLine("Enter in a animal to see the time of that animals exhibition");
-            //Console.ReadLine();
+            Console.WriteLine("5. Redback Spider        " + "Gardens North Side         ");
+            Console.WriteLine("6. Snow Leopard          " + "Arctic South Side          ");
+            Console.WriteLine("7. Anaconda              " + "Plains South Side          ");
+            Console.WriteLine("8. Alligator             " + "Lakes West Side            ");
+            Console.WriteLine("9. Polar Bear            " + "Arctic North Side          ");
+            Console.Write("\n");
+            Console.WriteLine("Enter in a animal to see the time of that animals exhibition");
+            Console.ReadLine();
+            string animal = Console.ReadLine();
             //Console.WriteLine("Hello World!");
             Console.ReadLine();
         }
